72143b7510034e0a8ad8ce9467cb2ab51faa4d0e
morwen80 <ribesnero80@gmail.com>
2019-06-01 19:26:53 +0200

    play again button working

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
+resetButton = document.querySelector('#reset')
+resetButton.addEventListener('click', playAgain);
+
 const memoryCards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard(){
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
-//     first click
     hasFlippedCard = true;
     firstCard = this;
   }
   else {
-//     second click
     hasFlippedCard = false;
     secondCard = this
-
 //     do cards match?
    checkForMatch();
   }
@@ -29,13 +33,48 @@ function checkForMatch(){
 function disableCards(){
   firstCard.removeEventListener('click',flipCard);
   secondCard.removeEventListener('click',flipCard);
+
+  resetBoard();
 }
 
 function unflipCards(){
+  lockBoard = true;
   setTimeout( () => {
     firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip')
+    secondCard.classList.remove('flip');
+
+    resetBoard();
    }, 1500)
 }
 
+function resetBoard(){
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+function playAgain(){
+  shuffle();
+  memoryCards.forEach( card => card.classList.remove('flip'));
+  memoryCards.forEach( card => card.addEventListener('click', flipCard));
+}
+
+function shuffle(){
+  memoryCards.forEach( card => {
+    let randomCard = Math.floor(Math.random() * 12);
+    card.style.order = randomCard;
+  });
+}
+
+
+// (function shuffle(){
+//   memoryCards.forEach( card => {
+//     let randomCard = Math.floor(Math.random() * 12);
+//     card.style.order = randomCard;
+//   });
+// })();
+
 memoryCards.forEach( card => card.addEventListener('click', flipCard));
+
+shuffle();
