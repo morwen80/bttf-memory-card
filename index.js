0a5916743c9d517452d562e60e0e3c6e3cb26c7a
morwen80 <ribesnero80@gmail.com>
2019-06-04 21:55:26 +0200

    change the playAgain function to a simple page reload

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const finalTime = document.querySelector('#finalTime');
 const memoryCards = document.querySelectorAll('.card');
 
 let isModalOpen = false;
-// let timer = 0;
-let isTimerStarted = false;
+let isGameStarted = false;
+let isTimerRunning = false;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
@@ -26,23 +26,28 @@ let firstCard, secondCard;
 const gifArray = [];
 
 function flipCard(){
+  isGameStarted = true;
+  isTimerRunning = true;
   if (lockBoard) return;
   if (this === firstCard) return;
 
   this.classList.add('flip');
 
+
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
-    startTimer();
+
   }
   else {
     hasFlippedCard = false;
     secondCard = this
+
 //     do cards match?
    checkForMatch();
    openModal();
   }
+
 };
 
 function checkForMatch(){
@@ -51,6 +56,7 @@ function checkForMatch(){
 }
 
 function disableCards(){
+    isGameStarted = true;
   firstCard.removeEventListener('click',flipCard);
   secondCard.removeEventListener('click',flipCard);
 
@@ -59,12 +65,13 @@ function disableCards(){
 
 function unflipCards(){
   lockBoard = true;
+  isGameStarted = true;
   setTimeout( () => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
 
     resetBoard();
-   }, 1500)
+  }, 1000)
 }
 
 function resetBoard(){
@@ -72,11 +79,20 @@ function resetBoard(){
   lockBoard = false;
   firstCard = null;
   secondCard = null;
+  isGameStarted = true;
 }
+
+// function playAgain(){
+//   location.reload();
+// }
 
 function playAgain(){
   shuffle();
+  isTimerRunning = false;
+  isGameStarted = true;
+  stopTimer();
   resetTimer();
+
   memoryCards.forEach( card => card.classList.remove('flip'));
   memoryCards.forEach( card => card.addEventListener('click', flipCard));
 }
@@ -87,6 +103,8 @@ function spinningIcon(){
 }
 
 function shuffle(){
+  isTimerRunning = false;
+
   memoryCards.forEach( card => {
     let randomCard = Math.floor(Math.random() * 12);
     card.style.order = randomCard;
@@ -95,6 +113,7 @@ function shuffle(){
 
 // MODAL
 function openModal(){
+  isGameStarted = false;
   isModalOpen = true;
   if (document.querySelectorAll('.flip').length == 6) {
   timerResult();
@@ -106,9 +125,9 @@ function openModal(){
 function closeModal(){
   if (isModalOpen) {
     modal.style.visibility = "hidden";
-    // timerResult();
-    resetTimer();
 
+    resetTimer();
+    playAgain();
   }
   isModalOpen = false;
 };
@@ -121,28 +140,33 @@ let minute = 0;
 let howLong;
 
 function startTimer(){
-  if (document.querySelectorAll('.flip').length !== 0){
-  timerBtn.innerHTML = `${minute}: ${second}
-  <i class="fas fa-hourglass-start rotate"></i>
-  `
-  second++;
-  if(second == 60){
-      minute++;
-      second = 0;
+  isTimerRunning = true;
+
+  if (isGameStarted == true ){
+
+    timerBtn.innerHTML = `${minute}: ${second}
+    <i class="fas fa-hourglass-start rotate"></i>
+    `
+    second++;
+      if(second == 60){
+        minute++;
+        second = 0;
+      }
     }
-  }
 }
 
 howLong = setInterval(startTimer, 1000);
 
 function stopTimer(){
+  isTimerRunning = false;
+  isGameStarted = false;
   clearInterval(howLong)
 }
 
 
 
 // function startTimer(){
-//   isTimerStarted = true;
+//   isTimerRunning = true;
 //     howLong = setInterval(function(){
 //         timerBtn.innerHTML = `${minute}: ${second}
 //         <i class="fas fa-hourglass-start rotate"></i>
@@ -165,12 +189,15 @@ function timerResult(){
 }
 
 function resetTimer(){
+  isGameStarted = false;
+  isTimerRunning = false;
+  // second = 0;
+  // minute = 0;
   timerBtn.innerHTML = `
     timer <i class="fas fa-hourglass-start"></i>
   `
-    clearInterval(howLong)
+    // clearInterval(howLong);
 };
 
 memoryCards.forEach( card => card.addEventListener('click', flipCard));
-
 shuffle();
